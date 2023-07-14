7fe1a12cb1eb2f6f7c27b897b4f4c974a630a6c9
avinashmishra <avinashm720@gmail.com>
2023-07-14 07:00:16 +0530

    Initial Commit

---
+++ app/src/main/java/com/deepshooter/composeclock/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.deepshooter.composeclock
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.deepshooter.composeclock.ui.theme.ComposeClockTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComposeClockTheme {
+
+            }
+        }
+    }
+}
+
+
