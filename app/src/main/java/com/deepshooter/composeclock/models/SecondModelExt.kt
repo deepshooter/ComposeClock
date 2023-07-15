b4d68c915cd1bb3ec3f62343038f6d63e6b2696b
avinashmishra <avinashm720@gmail.com>
2023-07-15 13:33:51 +0530

    Add SecondModel and SecondModelExt

---
+++ app/src/main/java/com/deepshooter/composeclock/models/SecondModelExt.kt
@@ -0,0 +1,28 @@
+package com.deepshooter.composeclock.models
+
+import com.deepshooter.composeclock.utils.SystemClock
+
+
+fun SecondModel.next(period: Long): SecondModel =
+    copy(
+        state = SecondModel.State(
+            seconds = (state.seconds + ((state.millis + period) / 1000)) % 60,
+            millis = (state.millis + period) % 1000,
+        )
+    )
+
+
+fun SecondModel.Companion.create(
+    systemClock: SystemClock,
+    style: SecondModel.Style = SecondModel.Style.Normal
+) =
+    systemClock.currentTimeMillis()
+        .let {
+            SecondModel(
+                style = style,
+                state = SecondModel.State(
+                    seconds = (it / 1000) % 60,
+                    millis = (it % 1000)
+                )
+            )
+        }
